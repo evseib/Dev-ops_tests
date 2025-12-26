efec8045b2baaef343f29cb2d4667b8ff2c6eee6
evseib <evertonseib@gmail.com>
2025-12-26 16:13:01 -0300

    First commit
    Vagrantfile working with rocky9 and apache

---
+++ vagran-lab/provision.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Configuring Apache..."
+
+sudo dnf install -y httpd
+
+# Desativa página de teste (caso ainda ativa)
+sudo mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.disabled 2>/dev/null || true
+# Correct permissions and SELinux context for synchronized files.
+sudo chown -R apache:apache /var/www/html
+sudo restorecon -R /var/www/html
+
+#Allow firewall access and start the service.
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --reload
+sudo systemctl enable httpd
+sudo systemctl restart httpd
+
+echo "Apache ready."
